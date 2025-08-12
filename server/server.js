b88bae738cceb987561e4d7274bbc556b0e891a9
bubby201 <156939137+bubby201@users.noreply.github.com>
2025-08-12 22:32:09 +0530

    update api

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const path = require('path');
 
 app.use(cors({
-  origin: 'https://pixel-forge-everd7ph5-bhavana-201s-projects.vercel.app' // your actual Vercel deployment URL
+   origin: 'https://pixel-forge-everd7ph5-bhavana-201s-projects.vercel.app' 
 }));
 
+app.get('/', async (req, res) => {
+ res.json({
+   message: 'Welcome to the backend server!'
+ });
+});
 
-// The catch-all route for the Single Page Application.
-// This route should not be changed.
 app.get('/back', async (req, res) => {
-  res.json({
-    message: 'This is the backend response from the server.',
-  });
+   res.json({
+  message: 'This is the backend response from the server.'
+ });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
