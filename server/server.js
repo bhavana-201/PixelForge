37253b8a488286d53293ebb860308f7ceef9160f
bubby201 <156939137+bubby201@users.noreply.github.com>
2025-08-12 22:15:09 +0530

    Configure backend for render deploy

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 
-// Serve the static files from the 'dist' folder.
-// Ensure there is a dot between express and static.
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(cors({
+  origin: 'https://pixel-forge-everd7ph5-bhavana-201s-projects.vercel.app' // your actual Vercel deployment URL
+}));
+
 
 // The catch-all route for the Single Page Application.
 // This route should not be changed.
-app.get('/*all', async (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.get('/back', async (req, res) => {
+  res.json({
+    message: 'This is the backend response from the server.',
+  });
 });
 
-const PORT = process.env.PORT || 5174;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
